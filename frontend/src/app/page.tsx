64229697e37916a87f4e1b02fd4b1a8738bac2ad
Aurélien DUPONTEIL <aurelienduponteil@gmail.com>
2026-04-03 16:53:10 +0200

    ajout de la section de présentation de l'entreprise

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 import MainSection from "@/components/home/MainSection";
+import PresentationSection from "@/components/home/PresentationSection";
 
 //Page d'accueil
+//Chaque section est développée dans un composant dans @/components/home
 export default function Home() {
   return (
     <main>
       {/*Section principale : titre, image, CTA*/}
       <MainSection />
+      {/*Section présentation : histoire et promesse*/}
+      <PresentationSection />
     </main>
   );
 }
---
+++ frontend/src/components/home/PresentationSection.tsx
@@ -0,0 +1,44 @@
+const presentation: Record<string, string>[] = [
+  {
+    title: "Notre histoire",
+    text: "Fondée à Bordeaux, Vite & Gourmand met à l'honneur à la gastronomie bordelaise depuis 2001.",
+    text2: "A l'origine, Vite & Gourmand était une petite adresse que les bordelais se transmettaient de bouche à oreille.",
+    bg: "bg-white",
+    text3: "Au fil des années, les recettes se sont affinées, la clientèle s'est agrandie et ce qui était un projet artisanal est devenu une référence incontournable de la restauration évènementielle à Bordeaux."
+  },
+  {
+    title: "Notre promesse",
+    text: " Depuis 25 ans, Julie et José ont bâti leur réputation sur une promesse simple : la satisfaction client avant tout.",
+    text2: "Aucun compromis sur la qualité, aucune prestation sans passion. Produits locaux, préparation le jour même, livraison soignée.",
+    bg: "bg-warm",
+  },
+];
+
+export default function PresentationSection() {
+  return (
+    <>
+      < section className="grid grid-cols-1 md:grid-cols-2" >
+
+        {/* On récupère les données de présentation avec .map */}
+        {presentation.map((pres) => (
+          <div key={pres.title}
+            className={`${pres.bg} px-10 lg:px-20 py-16 border-r border-cream`}>
+            <div className="flex items-center gap-3 mb-10">
+              <span className="inline-block w-8 h-px bg-copper" />
+              <span className="text-[12px] tracking-[4px] uppercase text-copper">
+                {pres.title}
+              </span>
+            </div>
+
+            <div className="italic leading-[1.85]">
+              <p className="py-4">{pres.text}</p>
+              <p className="py-4">{pres.text2}</p>
+              <p className="py-4">{pres.text3}</p>
+            </div>
+          </div>
+
+        ))}
+      </section >
+    </>
+  );
+}
